a6738113ebf449dc4b2331861c4985d70af67915
sagar guvvala <sagarreddyguvvala@gmail.com>
2022-07-23 17:01:53 +0530

    added angular material to our project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hospitalV14Angular';
 }
+
+// login component ->  (Authentication module)
+// radiologist component  (Radiology module)
+// receptionist component, patient appointment component , appointment edit search filter comp
+// ->  (Receptionist module  )
+// pathologist component -> (pathologist module)
+// admin component -> (admin module)
+// doctor component -> (doctor module)
+// 
